eb238d9f9f52e6658d7cddc71c99b87f2e0d189b
bilwal <bilawal.mehmood@sapphire.co>
2024-08-21 14:44:20 +0500

    1- Modified Home Screen Design. 2- Design Home Card and fetch List with dummy data.

--- lib/controllers/home_screen_controller.dart
+++ lib/controllers/home_screen_controller.dart
@@ -13,4 +13,38 @@ class HomeScreenController extends GetxController {
     }
   }
 
+  List<Map<String,dynamic>> homeCards = [
+    {
+      "title": "Plate Category",
+      "iconPath": "assets/icons/plate-1.png",
+      "subTitle": "PLATINUM",
+    },
+    {
+      "title": "Desired Charachters",
+      "iconPath": "assets/icons/plate-2.png",
+      "subTitle": "AHMED07",
+    },
+    {
+      "title": "Bid Start Date and Time",
+      "iconPath": "assets/icons/calander.png",
+      "subTitle": "02 JAN 2024 | 11:59",
+    },
+    {
+      "title": "Bid End Date and Time",
+      "iconPath": "assets/icons/calander.png",
+      "subTitle": "02 FEB 2024 | 11:59",
+    },
+    {
+      "title": "Starting bid amount",
+      "iconPath": "assets/icons/price.png",
+      "subTitle": "11,123,1",
+    },
+    {
+      "title": "Current highest bid",
+      "iconPath": "assets/icons/auction.png",
+      "subTitle": "1,231,1",
+    }
+   
+  ];
+
 }
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,10 +1,10 @@
 /* Created By Bilawal Mehmood  on 25-JAN-2024*/
+import 'package:excise_e_auction/ui/custom_widgets/general_button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:excise_e_auction/controllers/home_screen_controller.dart';
 import 'package:excise_e_auction/ui/custom_widgets/custom_scaffold.dart';
 import 'package:excise_e_auction/utils/app_colors.dart';
-import 'package:excise_e_auction/utils/constants.dart';
 
 class HomeScreen extends GetView<HomeScreenController> {
   const HomeScreen({super.key});
@@ -26,108 +26,67 @@ class HomeScreen extends GetView<HomeScreenController> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              publicDashboardCard(title: "Programs",shadowColor: const Color(0xff0F663E), iconPath: 'assets/icons/programs.png',onTap: ()=>controller.onHomeCardTap("Programs")),
-              const SizedBox(width: 25),
-              publicDashboardCard(title: "Check Eligibility",shadowColor: const Color(0xffFF722C),iconPath: 'assets/icons/check_eligbility.png',onTap: ()=>controller.onHomeCardTap("Check Eligibility")),
-            ]
+          Container(
+            width: Get.width*0.4,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+             gradient: LinearGradient(
+               colors: [kPrimaryColor,kPrimaryColor.withOpacity(0.8)],
+               begin: Alignment.topCenter,
+               end: Alignment.bottomCenter
+             ),
+              borderRadius: BorderRadius.circular(10) 
+            ),
+            child: Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Auction ID",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500,color: Colors.white.withOpacity(0.5)),),
+                const Text("0011234",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w500,color: Colors.white),),
+              ],
+            )),
           ),
-          const SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [ 
-              publicDashboardCard(title: "Registration",shadowColor: const Color(0xff0566B1),iconPath: 'assets/icons/registration.png',onTap: ()=>controller.onHomeCardTap("Registration")),
-              const SizedBox(width: 25),
-              publicDashboardCard(title: "Complaints",shadowColor: const Color(0xff1FB2BC),iconPath: 'assets/icons/complaints.png',onTap: ()=>controller.onHomeCardTap("Complaints")),],
+          SizedBox(height: Get.height*0.05),
+          GridView.builder(
+            shrinkWrap: true,
+            itemCount: controller.homeCards.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1.2, crossAxisSpacing: 10,mainAxisSpacing: 20),
+            itemBuilder: (context,index){
+              return homeCard(title: controller.homeCards[index]["title"],subTitle: controller.homeCards[index]["subTitle"],iconPath: controller.homeCards[index]["iconPath"],onTap: (){});
+            }
           ),
-          SizedBox(height: Get.height*0.1),
-          homeButton(),
+          SizedBox(height: Get.height*0.05),
+          GeneralButton(onPressed: (){},text: "Place a Bid",color: kBlackColor,)
         ],
       ),
     );
   }
 
-   Widget publicDashboardCard({String title="Title",Color shadowColor=Colors.purple, String iconPath='assets/icons/programs.png', required Function() onTap}) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: onTap,
+   Widget homeCard({String title="Title", String subTitle="Title", String iconPath='assets/icons/l_plate.png', required Function() onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: Get.width,
+        padding: const EdgeInsets.all(8),
+        decoration:  BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5) ,
+          boxShadow:  const [
+            BoxShadow(color: Colors.black12,blurRadius: 7,offset: Offset(2, 2)),
+            BoxShadow(color: Colors.black12,blurRadius: 7,offset: Offset(-2, -2))
+            ]
+        ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              height: 160,
-              decoration: BoxDecoration(color: const Color(0xffF4F3FF),borderRadius: BorderRadius.circular(20),
-                boxShadow: [BoxShadow(color: shadowColor,blurRadius: 0,offset: const Offset(15, 15))]),
-              margin: const EdgeInsets.only(right: 15,top: 20),
-              padding: const EdgeInsets.all(15),
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(20),
-                boxShadow:  const [BoxShadow(color: Colors.black12,blurRadius: 10,offset: Offset(0, 10))]),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(iconPath,height: 60,width: 60),
-                    const SizedBox(height: 10),
-                    Text(title,textAlign: TextAlign.center,overflow: TextOverflow.ellipsis,maxLines: 2,
-                      style:  const TextStyle(fontSize: 13,fontWeight: FontWeight.w500,color: Color(0xff5B5B5B)))
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              height: 25,
-              width: 3,
-              color: shadowColor,
-            ),
-            Container(
-              height: 12,
-              width: 12,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: shadowColor,
-              ),
-            )
+            Image.asset(iconPath,height: 30,width: 30, fit: BoxFit.contain),
+            Text(title,textAlign: TextAlign.center,overflow: TextOverflow.ellipsis,maxLines: 2,
+              style:  const TextStyle(fontSize: 12,fontWeight: FontWeight.w400,color: kPrimaryColor)),
+            Text(subTitle,textAlign: TextAlign.center,overflow: TextOverflow.ellipsis,maxLines: 2,
+              style:  const TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: kBlackColor))
           ],
         ),
-      ),
-    );
-  }
-
-  GestureDetector homeButton() {
-    return GestureDetector(
-      onTap: ()=>Get.toNamed(kLoginScreenRoute),
-      child: Container(width: double.infinity, height: 54, padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30),
-            gradient: const LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-                Color(0xff352C7B),
-                Color(0xff0F9C4D),
-              ],
-            ),
-          ),
-        child:  Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/icons/lock_icon.png'),
-            const SizedBox(width: 5),
-            const Text(
-              "Employer Login",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w500,
-                color: kWhiteColor,
-              ),
-            ),
-          ],
-        )
       ),
     );
   }
